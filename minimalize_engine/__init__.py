6c6b58de91049c0f6cbcff22301c8d573344354b
Liz_hookah <57042742+watarionn@users.noreply.github.com>
2026-09-08 11:34:49 +0900

    feat: expose Rinka target-style API

--- minimalize_engine/__init__.py
+++ minimalize_engine/__init__.py
@@ -1,4 +1,15 @@
 from .config import MinimalizeConfig
 from .pipeline import minimalize
+from .target_style import (
+    apply_rinka_reference_style,
+    minimalize_rinka_reference,
+    rinka_reference_config,
+)
 
-__all__ = ["MinimalizeConfig", "minimalize"]
+__all__ = [
+    "MinimalizeConfig",
+    "minimalize",
+    "rinka_reference_config",
+    "apply_rinka_reference_style",
+    "minimalize_rinka_reference",
+]
